64c2d646b5cd03e2f2d31588bb1221f020770f5a
Dawid Kiciński <kicinski.dawid@gmail.com>
2020-04-06 15:19:02 +0200

    Probably i should stick with std::vector

--- src/tensor1d.hpp
+++ src/tensor1d.hpp
@@ -1,19 +1,38 @@
 #pragma once
 
+#include <algorithm>
+#include <cstddef>
+#include <initializer_list>
+
 namespace space {
 template <typename DataType> class Tensor1D {
+
   private:
-    DataType *_data;
+    DataType *_data{};
 
   public:
-    int length;
+    size_t length{};
 
-  public:
-    explicit Tensor1D(int length) : _data(new DataType[length]), length(length) {}
+    explicit Tensor1D(size_t length) : _data(new DataType[length]), length(length) {}
+
+    Tensor1D(std::initializer_list<DataType> list) : Tensor1D(list.size()) {
+        int count = 0;
+        for (auto element : list) {
+            _data[count] = element;
+            ++count;
+        }
+    }
+
     ~Tensor1D() { delete[] _data; }
 
-    DataType & operator[](int index) {
-        return _data[index];
+    Tensor1D(Tensor1D const &) = delete; // to avoid shallow copies
+
+    Tensor1D &operator=(Tensor1D const &) = delete; // to avoid shallow copies
+
+    DataType &operator[](int index) { return _data[index]; }
+
+    bool operator==(Tensor1D<DataType> const &tensor_r) {
+        return std::equal(_data, std::end(_data), std::begin(tensor_r));
     }
 };
 } // namespace space
