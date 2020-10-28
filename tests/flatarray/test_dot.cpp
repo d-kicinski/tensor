b96f931fbf6ad2c763d07b718de3143828f7f212
Dawid Kiciński <kicinski.dawid@gmail.com>
2020-10-28 19:05:48 +0100

    Remove whitespaces :^)

--- tests/flatarray/test_dot.cpp
+++ tests/flatarray/test_dot.cpp
@@ -10,6 +10,7 @@ TEST_CASE("dot: Matrix[2, 3] X Vector[3]")
     Vector vector = {1, 1, 1};
     Vector result = dot(matrix, vector);
     Vector expected_result = {3, 6};
+
     REQUIRE(expected_result == result);
 }
 
@@ -35,9 +36,7 @@ TEST_CASE("dot: Matrix[3, 3] X Matrix[3, 3]")
         {1, 5, 9},
         {2, 6, 5}
     };
-
     Matrix result = dot(matrix, matrix);
-
     Matrix expected_result = {
         {16, 26, 33},
         {26, 80, 93},
@@ -53,15 +52,12 @@ TEST_CASE("dot: Matrix[2, 3] X Matrix[3, 2]")
         {3, 1, 3},
         {1, 5, 9},
     };
-
     Matrix matrixB = {
         {3, 1},
         {1, 5},
         {2, 6}
     };
-
     Matrix result = dot(matrixA, matrixB);
-
     Matrix expected_result = {
         {16, 26},
         {26, 80},
