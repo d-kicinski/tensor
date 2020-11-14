375e8df94b51957b0e2d8953f5f910d7945db11d
dkicinski <kicinski.dawid@gmail.com>
2020-11-14 01:02:12 +0100

    Add ops for Loss class

--- src/tensor/nn/loss.cpp
+++ src/tensor/nn/loss.cpp
@@ -1,1 +1,14 @@
 #include "loss.hpp"
+
+auto ts::Loss::operator()(const ts::Matrix &probs, ts::Tensor<int, 1> const & labels) -> float
+{
+    return forward(probs, labels);
+}
+
+auto ts::Loss::forward(const ts::Matrix &probs, ts::Tensor<int, 1> const & labels) -> float
+{
+    _labels = labels;
+    auto log_probs = -ts::log(ts::get(probs, labels));
+
+    // compute the loss: average cross-entropy loss and regularization
+}
