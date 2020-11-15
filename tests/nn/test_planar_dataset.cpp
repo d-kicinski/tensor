ee2709b3f8810fbcdb9314fd5920da53e1050a23
Dawid Kiciński <kicinski.dawid@gmail.com>
2020-11-15 23:10:34 +0100

    Implement really basic input data abstraction

---
+++ tests/nn/test_planar_dataset.cpp
@@ -0,0 +1,29 @@
+#include <catch2/catch.hpp>
+#include <tensor/nn/planar_dataset.hpp>
+
+TEST_CASE("load dataset")
+{
+    auto dataset = ts::PlanarDataset("resources/test_planar_data.tsv", true);
+
+    REQUIRE(dataset.inputs().shape() == std::array{6, 2});
+    REQUIRE(dataset.labels().shape() == std::array{6});
+}
+
+TEST_CASE("dataset iterator")
+{
+    auto dataset = ts::PlanarDataset("resources/test_planar_data.tsv", true, 2);
+    float i = 0;
+    for (auto [inputs, labels] : dataset) {
+        REQUIRE(inputs.shape() == std::array{2, 2});
+        REQUIRE(labels.shape() == std::array{2});
+
+        ts::Tensor<int, 1> expected_labels = {(int)i, (int)i};
+        ts::Tensor<float, 2> expected_inputs = {{i+2, i+1},
+                                                {i+2, i+1}};
+
+        REQUIRE(inputs == expected_inputs);
+        REQUIRE(labels == expected_labels);
+        ++i;
+    }
+    REQUIRE(i == 3);
+}
