24d459dd896754c45630f2a2b4dc3adecc6bcc7a
Dawid Kiciński <kicinski.dawid@gmail.com>
2020-11-30 20:43:47 +0100

    Run test only when they do exist

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,7 @@ BUILD_DIR=cmake-build
 rm -r ${BUILD_DIR}
 cmake -H. -B${BUILD_DIR}
 cd ${BUILD_DIR} && make -j12
-cd bin && ./tests
-cd ../..
+if [ -f ${BUILD_DIR}/bin/tests ]; then
+  cd bin && ./tests && cd ..
+fi
+cd ..
