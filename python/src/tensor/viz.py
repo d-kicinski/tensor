ed53f3c7703ba499b6d4aa706dc0f2a3da076590
Dawid Kiciński <kicinski.dawid@gmail.com>
2021-02-12 01:46:09 +0100

    Use id as it's more robust in comparison to current solution

--- python/src/tensor/viz.py
+++ python/src/tensor/viz.py
@@ -10,33 +10,19 @@ from graphviz import Digraph
 def viz(variable: tsg.Variable, filename: str) -> Digraph:
     f = Digraph(filename=filename)
     f.attr(rankdir='LR')
-
     f.attr('node', shape='rectangle')
-
-    register: Dict[str, int] = defaultdict(int)
 
     def loop(var: tsg.Variable):
         if var.op:
+            f.node(str(id(var)), f"{str(var)}\n{var.value.shape}")
+            f.node(str(id(var.op)), str(var.op), shape="oval")
+            f.edge(str(id(var.op)), str(id(var)))
 
-            var_id = f"{str(var)} {var.value.shape} #{register[str(var)]}"
-            op_id = f"{str(var.op)} #{register[str(var.op)]}"
-
-            register[str(var)] += 1
-            register[str(var.op)] += 1
-
-            f.node(var_id, f"{str(var)}\n{var.value.shape}")
-            f.node(op_id, str(var.op), shape="oval")
-            f.edge(op_id, var_id)
-
-            var.op.backward(var.grad)
             if inputs := var.op.inputs:
                 i: tsg.Variable
                 for i in inputs:
-
-                    var_id = f"{str(i)} {i.value.shape} #{register[str(i)]}"
-
-                    f.node(var_id, f"{str(i)}\n{i.value.shape}")
-                    f.edge(var_id, op_id)
+                    f.node(str(id(i)), f"{str(i)}\n{i.value.shape}")
+                    f.edge(str(id(i)), str(id(var.op)))
                     loop(i)
     loop(variable)
 
