f510cbfc96496519be2bddedee0a6b07ef0f6492
Dawid Kiciński <kicinski.dawid@gmail.com>
2021-02-26 14:29:52 +0100

    Forward declare templates from activations.cpp
    This may fix failing build in TravisCI, will see ...

--- cpp/src/tensor/nn/activations.cpp
+++ cpp/src/tensor/nn/activations.cpp
@@ -1,1 +1,8 @@
 #include "activations.hpp"
+
+template class ts::ActivationBase<float, 2>;
+template class ts::ActivationBase<float, 3>;
+template class ts::ReLU<float, 2>;
+template class ts::ReLU<float, 3>;
+template class ts::ActivationFactory<float, 2>;
+template class ts::ActivationFactory<float, 3>;
