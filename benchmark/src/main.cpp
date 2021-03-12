5ad0e34bdaa8330f49b9ad3eab4dbe463cc8f3c0
Dawid Kiciński <kicinski.dawid@gmail.com>
2021-03-12 20:07:48 +0100

    Add Linear and Conv2D layers to benchmark
    Conv2D is slow as fuck :/

--- benchmark/src/main.cpp
+++ benchmark/src/main.cpp
@@ -1,4 +1,6 @@
 #include <chrono>
+#include <tensor/nn/conv2d.hpp>
+#include <tensor/nn/feed_forward.hpp>
 #include <tensor/tensor.hpp>
 
 using namespace std::chrono;
@@ -10,10 +12,9 @@ auto create_matrices(int in, int middle, int out) -> std::pair<ts::MatrixF, ts::
     return std::make_pair(std::move(a), std::move(b));
 }
 
-auto benchmark(std::function<void(void)> const &fn) -> uint
+auto benchmark(std::function<void(void)> const &fn, size_t const runs = 1000) -> uint
 {
-    constexpr size_t runs = 10000;
-    std::array<uint, runs> results{};
+    std::vector<uint> results(runs);
 
     for (auto & r: results) {
         auto time_start = high_resolution_clock::now();
@@ -26,7 +27,7 @@ auto benchmark(std::function<void(void)> const &fn) -> uint
 
 auto benchmark_matrix_multiply() -> void
 {
-    std::cout << "with blas: " << std::endl;
+    std::cout << "ts::dot with blas: " << std::endl;
     {
         auto [a, b] = create_matrices(64, 512, 10);
         auto time = benchmark([a=std::ref(a), b=std::ref(b)](){ ts::blas::dot(a, b);} );
@@ -39,7 +40,7 @@ auto benchmark_matrix_multiply() -> void
         std::cout << "\t[128, 1024]x[1024, 10]: " << time << " us" << std::endl;
     }
 
-    std::cout << "with naive: " << std::endl;
+    std::cout << "ts::dot with naive: " << std::endl;
     {
         auto [a, b] = create_matrices(64, 512, 10);
         auto time = benchmark([a=std::ref(a), b=std::ref(b)](){ ts::naive::dot(a, b);} );
@@ -53,8 +54,52 @@ auto benchmark_matrix_multiply() -> void
     }
 }
 
+auto benchmark_linear() -> void
+{
+    auto input = ts::MatrixF(64, 512);
+    auto output = ts::MatrixF();
+    {
+        auto layer = ts::FeedForward::create(512, 10, ts::Activation::NONE);
+        auto time_fw = benchmark([&](){ output = layer(input); }, 10);
+        auto time_bw = benchmark([&](){ layer.backward(output); }, 10);
+        std::cout << "Linear::forward " << time_fw << " us" << std::endl;
+        std::cout << "Linear::backward " << time_bw << " us" << std::endl;
+    }
+    {
+        auto layer = ts::FeedForward::create(512, 10, ts::Activation::RELU);
+        auto time_fw = benchmark([&](){ output = layer(input); }, 10);
+        auto time_bw = benchmark([&](){ layer.backward(output); }, 10);
+        std::cout << "Linear[ReLU]::forward " << time_fw << " us" << std::endl;
+        std::cout << "Linear[ReLU]::backward " << time_bw << " us" << std::endl;
+    }
+}
+
+auto benchmark_conv2d() -> void
+{
+    auto input = ts::Tensor<float, 4>(16, 64, 64, 3);
+    auto output = ts::Tensor<float, 4>();
+    {
+        auto layer = ts::Conv2D::create(3, 24, 3, 1,
+                                        ts::Activation::NONE, false);
+        auto time_fw = benchmark([&](){ output = layer(input); }, 10);
+        auto time_bw = benchmark([&](){ layer.backward(output); }, 10);
+        std::cout << "Conv2D::forward " << time_fw << " us" << std::endl;
+        std::cout << "Conv2D::backward " << time_bw << " us" << std::endl;
+    }
+    {
+        auto layer = ts::Conv2D::create(3, 24, 3, 1,
+                                        ts::Activation::RELU, false);
+        auto time_fw = benchmark([&](){ output = layer(input); }, 10);
+        auto time_bw = benchmark([&](){ layer.backward(output); }, 10);
+        std::cout << "Conv2D[ReLU]::forward " << time_fw << " us" << std::endl;
+        std::cout << "Conv2D[ReLU]::backward " << time_bw << " us" << std::endl;
+    }
+}
+
 auto main() -> int
 {
     benchmark_matrix_multiply();
+    benchmark_linear();
+    benchmark_conv2d();
     return 0;
 }
