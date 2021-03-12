4dc2396187979c062107ec786e840c6197b23d19
Dawid Kiciński <kicinski.dawid@gmail.com>
2021-03-12 15:23:28 +0100

    Add evaluation to MNIST example (93% :D)

--- python/examples/mnist/main.py
+++ python/examples/mnist/main.py
@@ -1,7 +1,10 @@
+import time
 from typing import List
 
 import numpy as np
 import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 import tensor as ts
 from tensor.autograd import Variable
@@ -36,7 +39,9 @@ def train():
         transforms.Normalize((0.1307,), (0.3081,))
     ])
     dataset = datasets.MNIST('./data', train=True, download=True, transform=transform)
+    dataset2 = datasets.MNIST('./data', train=False, download=False, transform=transform)
     train_loader = torch.utils.data.DataLoader(dataset, batch_size=32)
+    test_loader = torch.utils.data.DataLoader(dataset2, batch_size=32)
 
     model = Net()
     loss_fn = ts.nn.CrossEntropyLoss()
@@ -44,6 +49,7 @@ def train():
     for w in model.weights():
         optimizer.register_params(w)
 
+    time_start = time.time()
     for batch_idx, (data, target) in enumerate(train_loader):
         x = ts.autograd.var(np.moveaxis(data.numpy(), 1, -1))
         y = ts.autograd.var(target.numpy())
@@ -57,6 +63,32 @@ def train():
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.value[0]))
+    time_end = time.time()
+    print(f"Training time: {time_end - time_start}")
+
+    time_start = time.time()
+    eval(model, test_loader, loss_fn)
+    time_end = time.time()
+    print(f"Evaluation time: {time_end - time_start}")
+
+
+def eval(model: Net, test_loader: DataLoader, loss_fn: ts.nn.CrossEntropyLoss):
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in tqdm(test_loader):
+            x = ts.autograd.var(np.moveaxis(data.numpy(), 1, -1))
+            y = ts.autograd.var(target.numpy())
+            output = model.forward(x)
+            test_loss += loss_fn(output, y).value[0]
+            pred = ts.argmax(output.value)
+            correct += np.sum(pred.numpy == y.value.numpy)
+
+    example_num = len(test_loader.dataset)  # type: ignore
+    test_loss /= example_num
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, example_num,
+        100. * correct / example_num))
 
 
 if __name__ == '__main__':
