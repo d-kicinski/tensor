d09e3989f1c9c8944d5b3b9c594db876557287cc
Dawid Kiciński <kicinski.dawid@gmail.com>
2021-03-12 10:45:35 +0100

    Implement kaiming_uniform weight init method

---
+++ cpp/src/tensor/nn/initialization.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <tensor/tensor.hpp>
+
+namespace ts {
+
+template <typename Element, int Dim>
+auto kaiming_uniform(std::vector<int> const &shape) -> Tensor<Element, Dim>
+{
+    std::default_random_engine random(69);
+    float fan_in = shape[0];
+
+    float gain = (float)(M_SQRT2) / std::sqrt(fan_in);
+    float std = gain / std::sqrt(fan_in);
+    float bound = std::sqrt(3.0f) * std;
+    std::uniform_real_distribution<Element> dist{-bound, bound};
+
+    std::array<int, Dim> array_shape;
+    std::copy(shape.begin(), shape.end(), array_shape.begin());
+    Tensor<Element, Dim> tensor(array_shape);
+    std::generate(tensor.begin(), tensor.end(), [&dist, &random]() { return dist(random); });
+    return tensor;
+}
+
+template <typename Element, int Dim>
+auto bias_init(std::vector<int> const &shape) -> Tensor<Element, Dim>
+{
+    std::default_random_engine random(69);
+    float fan_in = shape[0];
+
+    float bound = 1.0f / std::sqrt(fan_in);
+    std::uniform_real_distribution<Element> dist{-bound, bound};
+
+    std::array<int, Dim> array_shape;
+    std::copy(shape.begin(), shape.end(), array_shape.begin());
+    Tensor<Element, Dim> tensor(array_shape);
+    std::generate(tensor.begin(), tensor.end(), [&dist, &random]() { return dist(random); });
+    return tensor;
+}
+
+} // namespace ts
