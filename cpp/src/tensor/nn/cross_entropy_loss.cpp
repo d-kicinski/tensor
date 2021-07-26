3a4fcd0c45337c59ae1c61a5d55092425de93de5
Dawid Kiciński <kicinski.dawid@gmail.com>
2021-07-26 23:47:28 +0200

    Correctly use to_one_hot in CrossEntropyLoss

--- cpp/src/tensor/nn/cross_entropy_loss.cpp
+++ cpp/src/tensor/nn/cross_entropy_loss.cpp
@@ -12,15 +12,16 @@ auto ts::CrossEntropyLoss::forward(const ts::MatrixF &logits, ts::Tensor<int, 1>
     _scores = ts::softmax(logits);
 
     VectorF log_probs = ts::log(ts::get(_scores, labels));
-    float loss = -ts::sum(log_probs) / (log_probs.shape(0));
-    return loss;
+    return -ts::sum(log_probs) / static_cast<float>(log_probs.shape(0));
 }
 
 auto ts::CrossEntropyLoss::backward() -> ts::MatrixF
 {
     auto d_scores = ts::apply_if(
-        _scores, ts::to_one_hot(_labels), (Fn<float>)[](float e) { return e - 1.0f; });
-    d_scores = ts::apply(
-        d_scores, (Fn<float>)[&](float e) { return e / _scores.shape(0); });
+        _scores, ts::to_one_hot(_labels, _scores.shape(1)), (Fn<float>)[](float e) { return e - 1.0f; });
+    if (size_type batch_size = _scores.shape(0); batch_size > 1) {
+        d_scores = ts::apply(
+            d_scores, (Fn<float>)[&](float e) { return e / static_cast<float>(batch_size); });
+    }
     return d_scores;
 }
