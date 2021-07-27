4f4d8463e798cb0d99e4fbfe63da9b8712c08f98
Dawid Kiciński <kicinski.dawid@gmail.com>
2021-07-27 10:23:21 +0200

    Fix small bug in CrossEntropyLoss::backward

--- cpp/src/tensor/nn/cross_entropy_loss.cpp
+++ cpp/src/tensor/nn/cross_entropy_loss.cpp
@@ -18,7 +18,7 @@ auto ts::CrossEntropyLoss::forward(const ts::MatrixF &logits, ts::Tensor<int, 1>
 auto ts::CrossEntropyLoss::backward() -> ts::MatrixF
 {
     auto d_scores = ts::apply_if(
-        _scores, ts::to_one_hot(_labels, _scores.shape(1)), (Fn<float>)[](float e) { return e - 1.0f; });
+        _scores, ts::to_one_hot(_labels, _scores.shape(1) - 1), (Fn<float>)[](float e) { return e - 1.0f; });
     if (size_type batch_size = _scores.shape(0); batch_size > 1) {
         d_scores = ts::apply(
             d_scores, (Fn<float>)[&](float e) { return e / static_cast<float>(batch_size); });
