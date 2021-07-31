6a049c885670e872f7b6b38df1ec2ed9aef79fb0
Dawid Kiciński <kicinski.dawid@gmail.com>
2021-07-31 18:00:49 +0200

    Add few statistics utils

--- cpp/src/tensor/statistics.hpp
+++ cpp/src/tensor/statistics.hpp
@@ -1,14 +1,33 @@
 #pragma once
 
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <string>
+
+#include "tensor/data_holder.hpp"
+
 namespace ts {
+
+template <typename Element> auto mean(DataHolder<Element> const &v) -> float
+{
+    return std::accumulate(v.begin(), v.end(), 0.0f) / std::distance(v.begin(), v.end());
+}
+
+template <typename Element> auto max(DataHolder<Element> const &v) -> float
+{
+    return *std::max_element(v.begin(), v.end());
+}
+
+template <typename Element> auto min(DataHolder<Element> const &v) -> float
+{
+    return *std::min_element(v.begin(), v.end());
+}
 
 template <typename Element> auto print_stats(DataHolder<Element> const &v, std::string tag) -> void
 {
     std::cerr << tag << ":"
-              << " min = " << *std::min_element(v.begin(), v.end())
-              << " max = " << *std::max_element(v.begin(), v.end())
-              << " mean = " << std::accumulate(v.begin(), v.end(), 0.0f) / std::distance(v.begin(), v.end())
-              << std::endl;
+              << " min = " << min(v) << " max = " << max(v) << " mean = " << mean(v) << std::endl;
 };
 
-} // namespace
+} // namespace ts
